dd764750c29af22bde57c9fccb59cacd8a633395
Darkhobbo <darkhobbo@gmail.com>
2016-04-06 00:13:15 -0700

    Working on Bluetooth Socket Server.
    http://stackoverflow.com/questions/16676165/how-to-create-a-bluetooth-server-socket-in-android
    
    http://developer.android.com/guide/topics/connectivity/bluetooth.html#ManagingAConnection
    
    http://developer.android.com/guide/topics/connectivity/bluetooth.html

--- app/src/main/java/pancakeninjas/sliceproject/PrepareGameActivity.java
+++ app/src/main/java/pancakeninjas/sliceproject/PrepareGameActivity.java
@@ -1,18 +1,48 @@
 package pancakeninjas.sliceproject;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.UUID;
 
 /**
  * Created by darkhobbo on 4/5/2016.
  */
 public class PrepareGameActivity extends AppCompatActivity {
 
+    public BluetoothAdapter bluetoothAdapter;
+    private BluetoothServerSocket serverSocket;
+    private static final UUID SECURE_UUID = UUID.randomUUID();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.prepare_game_activity);
 
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(bluetoothAdapter==null){
+            Log.d("BluetoothAdapter","Bluetooth not supported");
+        }
+        else{
+            Intent discoverableIntent = new Intent(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION);
+            startActivity(discoverableIntent);
+            Log.d("Bluetooth", "Discoverable");
 
+            AcceptThread();
+        }
+    }
+
+    private void AcceptThread(){
+        BluetoothServerSocket tmp = null;
+        try{
+            //tmp = bluetoothAdapter.listenUsingRfcommWithServiceRecord("HELLO", SECURE_UUID);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
